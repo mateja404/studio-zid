e0ed1521c0bfdd6d8a04de34109ac26dfef5d0a8
mateja404 <markojovanovicc23@outlook.com>
2025-05-19 10:18:33 +0200

    - nasi projekti

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Reviews from "@/components/Reviews";
 import AboutUs from "@/components/AboutUs";
 import OurServices from "@/components/OurServices";
 import SpecificneUsluge from "@/components/SpecificneUsluge";
-import {Speaker} from "lucide-react";
+import OurProjects from "@/components/OurProjects";
 
 export default function Home() {
   return (
@@ -16,6 +16,7 @@ export default function Home() {
           <AboutUs/>
           <OurServices/>
           <SpecificneUsluge/>
+          <OurProjects/>
       </main>
     </div>
   );
--- components/OurProjects.tsx
+++ components/OurProjects.tsx
@@ -1,10 +1,26 @@
 import React from 'react';
+import Image from "next/image";
+import nasiprojekti from "@/public/NasiProjektiHome.png";
+import { MoveDownRight } from "lucide-react";
 
 const OurProjects = () => {
     return (
-        <div>
-            
-        </div>
+        <section className="w-full h-[1000px] xl:h-[800px] 2xl:h-[800px] bg-black text-white relative flex flex-col border-3 border-l-transparent border-r-transparent border-t-[#88664d] border-b-[#88664d]">
+            <Image src={nasiprojekti} alt={"Naši projekti"} className="w-full h-full object-cover absolute top-0 left-0 z-0"/>
+            <div className="w-full h-[70vw] flex items-center justify-center z-2">
+                <h1 className="text-9xl text-black">Naši <br/> Projekti</h1>
+            </div>
+            <div className="w-full h-[30vw] flex items-center justify-center z-2">
+                <div className="w-[90vw] h-[100px] mb-50 bg-[#b98d5e] flex items-center justify-center">
+                    <ul className="flex flex-row gap-x-30">
+                        <li className="flex flex-row gap-x-3 text-2xl text-black"><span className="w-[40px] h-[40px] bg-black rounded-full flex items-center justify-center"><MoveDownRight className="text-[#b98d5e]"/></span>Privatni Stanovi</li>
+                        <li className="flex flex-row gap-x-3 text-2xl text-black"><span className="w-[40px] h-[40px] bg-black rounded-full flex items-center justify-center"><MoveDownRight className="text-[#b98d5e]"/></span>Privatne Kuće</li>
+                        <li className="flex flex-row gap-x-3 text-2xl text-black"><span className="w-[40px] h-[40px] bg-black rounded-full flex items-center justify-center"><MoveDownRight className="text-[#b98d5e]"/></span>Lokali</li>
+                        <li className="flex flex-row gap-x-3 text-2xl text-black"><span className="w-[40px] h-[40px] bg-black rounded-full flex items-center justify-center"><MoveDownRight className="text-[#b98d5e]"/></span>Zgrade u izgradnji</li>
+                    </ul>
+                </div>
+            </div>
+        </section>
     );
 };
 
