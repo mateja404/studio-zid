bbeb83869cb7007ea19ee0108043a0585d802297
mateja404 <markojovanovicc23@outlook.com>
2025-05-19 09:15:19 +0200

    - specificne usluge

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,8 @@ import HomePage from "@/components/Home";
 import Reviews from "@/components/Reviews";
 import AboutUs from "@/components/AboutUs";
 import OurServices from "@/components/OurServices";
+import SpecificneUsluge from "@/components/SpecificneUsluge";
+import {Speaker} from "lucide-react";
 
 export default function Home() {
   return (
@@ -13,6 +15,7 @@ export default function Home() {
           <Reviews/>
           <AboutUs/>
           <OurServices/>
+          <SpecificneUsluge/>
       </main>
     </div>
   );
