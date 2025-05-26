245ed6a086ea4970a6b6b31579954a11f97f840b
mateja404 <markojovanovicc23@outlook.com>
2025-05-26 20:31:43 +0200

    commit za deploy

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,14 +1,17 @@
-import React from 'react';
+import React from "react";
 import { LoginForm } from "@/components/LoginForm";
 
-const Page = () => {
-    return (
-        <main className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
-            <div className="w-full max-w-sm">
-                <LoginForm/>
-            </div>
-        </main>
-    );
+const Page = async (props: { searchParams: Promise<{ callbackUrl?: string }> }) => {
+  const searchParams = await props.searchParams;
+  const callbackUrl = searchParams.callbackUrl || "/";
+
+  return (
+    <main className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+      <div className="w-full max-w-sm">
+        <LoginForm callbackUrl={callbackUrl} />
+      </div>
+    </main>
+  );
 };
 
 export default Page;
